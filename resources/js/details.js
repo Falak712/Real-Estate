7e5fe0669c15b2fe5d2f5fe1d367ca4130aa7ab6
ShahedDev26 <shahedtaffour.com>
2026-06-15 13:56:59 +0300

    Add frontend pages

--- resources/js/details.js
+++ resources/js/details.js
@@ -2,6 +2,14 @@
 // صفحة تفاصيل العقار
 // ==========================
 
+let description = document.getElementById("description");
+let space = document.getElementById("space");
+let rooms = document.getElementById("rooms");
+let baths = document.getElementById("baths");
+let direction = document.getElementById("direction");
+
+
+
 // 1. نجيب id من الرابط
 let params = new URLSearchParams(window.location.search);
 let id = params.get("id");
@@ -40,7 +48,22 @@ if (property) {
         document.getElementById("mainImage").src = property.images[0];
     }
 
-} else {
+    //موقع عالخريطة
+    let lat = property.pointOfWidth;
+    let lng = property.pointOfLength;
+    // إذا الإحداثيات موجودة
+    if (lat !== undefined && lng !== undefined) {
+       document.getElementById("map").src =
+          `https://www.google.com/maps?q=${lat},${lng}&output=embed`;
+     } else {
+      document.querySelector(".map-section").style.display = "none";
+   }
+
+
+
+}
+else {
     document.body.innerHTML =
         "<h2 style='text-align:center;color:red'>العقار غير موجود</h2>";
 }
+
